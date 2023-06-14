92244028215fb7541b597cc8273b3c469b6a314f
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-06-14 15:07:17 -0300

    Exercício 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const getSpeciesByIds = (...ids) => {
   // seu código aqui
+  if (!ids) return [];
+  const arraySpecieSpecific = [];
+  ids.forEach((id) => {
+    arraySpecieSpecific.push(data.species.filter((specie) => specie.id === id));
+  });
+  return arraySpecieSpecific.flat();
 };
 
 module.exports = getSpeciesByIds;
