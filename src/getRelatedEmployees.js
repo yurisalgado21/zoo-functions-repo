42c96c1c8c8773645d65ed588914941450743c9e
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-06-14 23:45:15 -0300

    Exercício 4 feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => data.employees
+  .some((people) => people.managers
+    .some((manager) => manager === id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const managedEmployee = data.employees
+    .filter((employee) => employee.managers.includes(managerId));
+  return managedEmployee
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 };
 
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
