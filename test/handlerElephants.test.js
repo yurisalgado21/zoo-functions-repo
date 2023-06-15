4b79f66ddce36435ac461e441a58412833a3c76a
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-06-15 15:27:56 -0300

    Exercício 6 feito

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,10 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se a função handlerElephants retorna o resultado esperado.', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toContain('Jefferson');
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5, 5);
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+});
