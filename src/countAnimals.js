33b1f22cb30d5973ac7811bc224969660cd40cf0
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-06-15 13:42:55 -0300

    Exercício 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const result = {};
+  if (typeof animal === 'object' && animal.sex) {
+    return data.species
+      .filter((specie) => specie.name === animal.species)
+      .map((specieSpecif) => specieSpecif.residents
+        .filter((resident) => resident.sex === animal.sex))
+      .flat().length;
+  } if (typeof animal === 'object' && !animal.sex) {
+    return data.species.filter((specie) => specie.name === animal.species)
+      .map((specieSpecif) => specieSpecif.residents)
+      .flat().length;
+  }
+  data.species.filter(({ name, residents }) => {
+    result[name] = residents.length;
+    return true;
+  });
+  return result;
 };
-
+console.log(countAnimals({ species: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
