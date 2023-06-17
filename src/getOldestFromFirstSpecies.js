03a4a19b2c78fa1a35fb8cf8b65e53b6bafc3ac3
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-06-17 20:23:52 -0300

    Exercicío 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employeSpecific = data.employees.find((employe) => employe.id === id);
+  const firstAnimalsResponsivels = employeSpecific.responsibleFor[0];
+  const animalOld = (idAnimal) => {
+    const animal = data.species.find((specie) => specie.id === idAnimal);
+    const animalResidentOld = animal.residents
+      .reduce((acc, curr) => (curr.age > acc.age ? curr : acc));
+    return Object.values(animalResidentOld);
+  };
+  return animalOld(firstAnimalsResponsivels);
 };
-
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
